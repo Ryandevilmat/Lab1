cac19333779b701e1230da6e29340a0e8ea090e9
RyanD <RyanD@DESKTOP-J2OT42D>
2024-01-18 18:57:13 -0500

    Lab1 complete

---
+++ src/csci1302/intro/HelloJava.java
@@ -0,0 +1,14 @@
+package csci1302.intro;
+
+/**
+ * A simple introductory Java class.
+ */
+public class HelloJava {
+   /**
+    * Print a silly message.
+    */
+   public static void main(String[] args) {
+       System.out.print("I am the 1337 h4x0r. Phear me!");
+   } // main(String[])
+
+} // class HelloJava
---
+++ src/csci1302/intro/SimpleMath.java
@@ -0,0 +1,7 @@
+package csci1302.intro;
+
+public class SimpleMath {
+    public static void main(String[] args) {
+    	System.out.print(3+4);
+    }
+}
